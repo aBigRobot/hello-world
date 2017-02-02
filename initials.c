15193cbc152dd0aab35b85d278681a2b883a22ea
Alex <alex@binarymastering.com>
2017-02-02 10:10:56 -0600

    Update initials.c

--- initials.c
+++ initials.c
@@ -1,3 +1,52 @@
+#include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(void);
+int main(void)
+{
+    // prompt user for their name
+    printf("Who you is?\n");
+    
+    string name = get_string();
+    int n = 0;
+    while (name[n] != '\0')
+    {
+        n++;
+    }
+    printf("%i\n", n);
+    
+     // make sure a string gets returned as a string
+     if (name != NULL)
+        {
+        // iterate over the characters of the string one at a time
+        for (int i = 0, l = strlen(name); i < l; i++)
+         {
+             if (name[i] >= 'a' && name[i] <= 'z')
+             {
+                 printf("%c\n", name[i] - ('a' - 'A'));
+             }
+             else
+             {
+             // print the character of the string one based of the i'th position of string
+              printf("%c\n", name[i]);
+             }
+
+         }
+        }
+
+    for (char c = 'A'; c <= 'Z'; c++)
+    {
+        // print f knows how to write a character as an integer value in ASCII
+        printf("%c is %i\n", c, c);
+    }
+
+    
+    
+    // determine where the first letter of each string, and then print the value of each letter.
+    
+    // print the initials
+    
+    
+    
+}
